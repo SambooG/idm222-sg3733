4a2cb51f211b6defcfeec1985b8c039142a9829f
Samantha Gaiser <sgaiser3@gmail.com>
2022-03-17 21:44:10 -0400

    working on design adjustments.

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ leftTarget.addEventListener("click", function(e){
         e.preventDefault();
     }
     const newPage = leftTarget.href;
-    const container = document.getElementsByClassName("landing-client-l")[0];
+    const container = document.getElementsByClassName("landing__client-l")[0];
     container.classList.add("grow");
     console.log(newPage)
     console.log(container)
     setTimeout(() => {
         window.location.href= newPage;
     }, 2000);
-})
+});
 
 const rightTarget = document.getElementById("right-target");
 
@@ -26,11 +26,34 @@ rightTarget.addEventListener("click", function(e){
         e.preventDefault();
     }
     const newPage = rightTarget.href;
-    const container = document.getElementsByClassName("landing-student-r")[0];
+    const container = document.getElementsByClassName("landing__student-r")[0];
     container.classList.add("r-grow");
     console.log(newPage)
     console.log(container)
     setTimeout(() => {
         window.location.href= newPage;
     }, 2000);
-})
+});
+
+// const buttonContainer = document.getElementById('button-container');
+//         const openButton = document.getElementById('open-button');
+//         const closeButton = document.getElementById('close-button');
+//         const mobileMenu = document.getElementById('mobile-menu');
+//         buttonContainer.addEventListener("click", () => {
+
+//             if (Array.from(mobileMenu.classList).includes('mobile-nav__list--open')) {
+//                 // When we're closing the menu
+//                 mobileMenu.classList.remove('mobile-nav__list--open');
+//                 mobileMenu.classList.add('mobile-nav__list--close');
+                
+//                 openButton.style.display = 'block';
+//                 closeButton.style.display = 'none';
+//             } else {
+//                 // When we're opening the menu
+//                 mobileMenu.classList.remove('mobile-nav__list--close');
+//                 mobileMenu.classList.add('mobile-nav__list--open');
+                
+//                 openButton.style.display = 'none';
+//                 closeButton.style.display = 'block';
+//             }
+//         });
